fa3311bf363133e9c57fb50cd5ddd7da538d8b6e
Memblers <47460233+Memblers@users.noreply.github.com>
2019-09-29 04:18:56 -0400

    initial import from 8bitworkshop.com

---
+++ mp3.s
@@ -0,0 +1,183 @@
+.export _mp3_command
+.import popa
+
+.macro beep
+        lda #%10011100
+        sta $4000
+        lda #%00010010
+        sta $4003
+
+.endmacro
+
+
+.segment "ZEROPAGE"
+; $00-$04 reserved for emergency interrupt (vectors are all $FF)
+
+   temp_x: .res 1
+   temp_y: .res 1
+   temp_lo: .res 1
+   temp_hi: .res 1
+   received: .res 1
+   temp_a:  .res 1
+   v5000:   .res 1
+   count_hi: .res 1
+   
+   x_param: .res 1
+.segment "CODE"
+
+;----
+mp3_send:
+   stx temp_x
+   sty temp_y
+   sta temp_a
+   lda v5000
+   and #$7F
+   sta v5000
+   sta $5000   ; start bit
+
+   ; 186.434 cycles/bit
+   ldx #33
+:
+   dex
+   bne :-
+   nop
+   ldy #8
+@loop:
+   lsr temp_a
+   ror
+   and #$80
+   ora v5000
+   sta $5000
+
+   ldx #32
+:
+   dex
+   bne :-
+   nop
+   nop
+
+   dey
+   bne @loop
+
+   inc temp_hi ; waste 5
+   nop
+   nop
+
+   lda v5000
+   ora #$80
+   sta $5000   ; stop bit
+
+   ldx #120 ;40
+:
+   dex
+   bne :-
+   ldy temp_y
+   ldx temp_x
+   rts
+
+;----
+   
+_mp3_command:
+	tax	; parameter 2
+        jsr popa
+        stx temp_y
+        jsr popa
+        tay
+        ldx temp_y
+        
+;mp3_command:
+	pha
+	lda #$7E
+	jsr mp3_send
+	lda #$FF
+	jsr mp3_send
+	lda #$06
+	jsr mp3_send
+	pla
+	jsr mp3_send
+	lda #0
+	jsr mp3_send
+	txa
+	jsr mp3_send
+	tya
+	jsr mp3_send
+	lda #$EF
+	jsr mp3_send
+	
+;	ldy #1
+;	jsr delay
+	rts
+	
+.enum
+	CMD_NEXT_SONG		= $01
+	CMD_PREVIOUS_SONG
+	CMD_TRACK_SELECT
+	CMD_VOLUME_UP
+	CMD_VOLUME_DOWN
+	CMD_SELECT_VOLUME
+	CMD_SELECT_EQ
+	CMD_SELECT_LOOPED_TRACK
+	CMD_SELECT_TF
+	CMD_SLEEP_MODE
+	CMD_WAKE_UP
+	CMD_CHIP_RESET
+	CMD_PLAY
+	CMD_PAUSE
+	CMD_SELECT_FILE	
+	CMD_LOOP			= $11
+	CMD_SELECT_MP3_FOLDER	= $12
+	CMD_ADVERT			= $13
+	CMD_STOP			= $16
+	CMD_PLAY_FOLDER_LOOP
+	CMD_SHUFFLE_PLAY
+	CMD_SINGLE_LOOP_ENABLE
+	CMD_PLAY_WITH_VOLUME	= $22
+.endenum
+	
+
+;cmd_next_song:				.byte $7E, $FF, $06, $01, $00, $00, $00, $EF
+;cmd_previous_song:			.byte $7E, $FF, $06, $02, $00, $00, $00, $EF
+;cmd_track_select:			.byte $7E, $FF, $06, $03, $00, $00, $00, $EF	; parameters: X = track number (1..255)
+;cmd_volume_up:				.byte $7E, $FF, $06, $04, $00, $00, $00, $EF
+;cmd_volume_down:			.byte $7E, $FF, $06, $05, $00, $00, $00, $EF
+;cmd_select_volume:			.byte $7E, $FF, $06, $06, $00, $00, $1E, $EF	; pass - parameters: X = volume (0..30)
+;cmd_select_eq:				.byte $7E, $FF, $06, $07, $00, $00, $01, $EF	; parameters: reserved
+;cmd_select_looped_track: 	.byte $7E, $FF, $06, $08, $00, $01, $03, $EF	; parameters: Y = folder number, X = track number
+;cmd_select_tf:				.byte $7E, $FF, $06, $09, $00, $00, $02, $EF	; pass - select micro SD
+;cmd_sleep_mode:			.byte $7E, $FF, $06, $0A, $00, $00, $00, $EF	; pass
+;cmd_wake_up:				.byte $7E, $FF, $06, $0B, $00, $00, $00, $EF	; fail
+;cmd_chip_reset:			.byte $7E, $FF, $06, $0C, $00, $00, $00, $EF	; pass - note: will cause audible pop
+;cmd_play:					.byte $7E, $FF, $06, $0D, $00, $00, $00, $EF
+;cmd_pause:					.byte $7E, $FF, $06, $0E, $00, $00, $00, $EF 
+;cmd_select_file:			.byte $7E, $FF, $06, $0F, $00, $01, $0E, $EF	; parameters: X = folder number, Y = track number
+;cmd_advert:				.byte $7E, $FF, $06, $13, $00, $01, $01, $EF	; pass - parameters: X = high byte, Y = low byte (1..3000)
+;cmd_stop:					.byte $7E, $FF, $06, $16, $00, $00, $00, $EF
+;cmd_play_folder_loop:		.byte $7E, $FF, $06, $17, $00, $01, $00, $EF 	; parameters: Y = folder number
+;cmd_shuffle_play:			.byte $7E, $FF, $06, $18, $00, $00, $00, $EF
+;cmd_single_loop_enable:	.byte $7E, $FF, $06, $19, $00, $00, $00, $EF 	; pass - parameter: Y = 0:enable 1:disble
+;cmd_play_with_volume:		.byte $7E, $FF, $06, $22, $00, $1E, $01, $EF	; parameters: Y = volume, X = track number
+
+
+;---- Y second delay
+delay:
+;   lda v5000
+;   ora #$40
+;   sta $5000
+
+   ldx #60
+:
+   lda $2002
+   bpl :-
+   dex
+   bne :-
+   ldx #60
+   dey
+   bne :-
+
+;   lda v5000
+;   and #%10111111
+;   sta $5000
+;   sta v5000
+   rts
+;----
+
---
+++ mp3player.c
@@ -0,0 +1,112 @@
+
+//#include <stdlib.h>
+//#include <string.h>
+
+
+// include NESLIB header
+#include "neslib.h"
+
+// include CC65 NES Header (PPU)
+#include <nes.h>
+
+// link the pattern table into CHR ROM
+//#link "chr_generic.s"
+
+
+typedef enum
+{
+  CMD_NEXT_SONG		= 0x01,
+  CMD_PREVIOUS_SONG,
+  CMD_TRACK_SELECT,
+  CMD_VOLUME_UP,
+  CMD_VOLUME_DOWN,
+  CMD_SELECT_VOLUME,
+  CMD_SELECT_EQ,
+  CMD_SELECT_LOOPED_TRACK,
+  CMD_SELECT_TF,
+  CMD_SLEEP_MODE,
+  CMD_WAKE_UP,
+  CMD_CHIP_RESET,
+  CMD_PLAY,
+  CMD_PAUSE,
+  CMD_SELECT_FILE,
+  CMD_LOOP			= 0x11,
+  CMD_SELECT_MP3_FOLDER	= 0x12,
+  CMD_ADVERT			= 0x13,
+  CMD_STOP			= 0x16,
+  CMD_PLAY_FOLDER_LOOP,
+  CMD_SHUFFLE_PLAY,
+  CMD_SINGLE_LOOP_ENABLE,
+  CMD_PLAY_WITH_VOLUME	= 0x22
+  } CMD;
+
+void __fastcall__ mp3_command(CMD command, unsigned char param1, unsigned char param2);
+
+byte pad1;
+byte spr_id;
+  
+
+/*{pal:"nes",layout:"nes"}*/
+const char PALETTE[32] = { 
+  0x03,			// screen color
+
+  0x11,0x30,0x27,0x0,	// background palette 0
+  0x1c,0x20,0x2c,0x0,	// background palette 1
+  0x00,0x10,0x20,0x0,	// background palette 2
+  0x06,0x16,0x26,0x0,   // background palette 3
+
+  0x16,0x35,0x24,0x0,	// sprite palette 0
+  0x00,0x37,0x25,0x0,	// sprite palette 1
+  0x0d,0x2d,0x3a,0x0,	// sprite palette 2
+  0x0d,0x27,0x2a	// sprite palette 3
+};
+
+// setup PPU and tables
+void setup_graphics() {
+  // clear sprites
+  oam_clear();
+  // set palette colors
+  pal_all(PALETTE);
+}
+
+void main(void)
+{
+  setup_graphics();
+
+  // draw message  
+  vram_adr(NTADR_A(2,2));
+  vram_write("MI MEDIA PLAYER",15);
+  vram_adr(NTADR_A(2,3));
+  vram_write(__DATE__ " - "__TIME__, 22);
+  // enable rendering
+  ppu_on_all();
+  // infinite loop
+  while(1)
+  {
+    ppu_wait_nmi();
+    pad1 = pad_trigger(0);    
+    
+    if (pad1 & PAD_RIGHT)
+      mp3_command(CMD_NEXT_SONG,0,0);    
+    if (pad1 & PAD_LEFT)    
+      mp3_command(CMD_PREVIOUS_SONG,0,0);    
+    if (pad1 & PAD_SELECT)    
+      mp3_command(CMD_SHUFFLE_PLAY,0,0);
+    if (pad1 & PAD_UP)
+      mp3_command(CMD_VOLUME_UP,0,0);
+    if (pad1 & PAD_DOWN)
+      mp3_command(CMD_VOLUME_DOWN,0,0);
+    if (pad1 & PAD_A)
+      mp3_command(CMD_SELECT_EQ,1,1);
+    if (pad1 & PAD_B)
+    {
+      spr_id = 0;
+      mp3_command(CMD_SELECT_EQ,2,2);
+      spr_id = oam_spr(0x18,0x90,'C',2,spr_id);
+      mp3_command(CMD_SELECT_EQ,2,2);        
+    }     
+    
+  }
+}
+
+//#link "mp3.s"
